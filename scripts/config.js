87ee079fd83c0110c7a5f30aafaffa2af0026e45
Nicholas Galen <galennicholas@proton.me>
2025-06-21 15:56:26 -0400

    gitignore update

---
+++ scripts/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    WEATHER_API_KEY: 'decb5ef8805ab9dcf39b2384797b0e8e',
+    EMAILJS_CONFIG: {
+        USER_ID: '9GZO8vbd-UW3uEDIF',
+        SERVICE_ID: 'service_wqh4l67',
+        TEMPLATE_ID: 'template_p2voew1'
+    }
+};
